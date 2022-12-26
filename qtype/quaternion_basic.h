75176daac3ba578801804618e1060e7db4079daa
Andrew Starritt <andrew.starritt@gmail.com>
2022-12-26 12:30:23 +1100

    Uppdated from GPL to LGPL - no functional changes.

--- qtype/quaternion_basic.h
+++ qtype/quaternion_basic.h
@@ -3,19 +3,19 @@
  * This file is part of the Python quaternion module. It privides basic
  * quaternion maths operation with minimalist reference to Python.
  *
- * Copyright (c) 2018-2022  Andrew C. Starritt
+ * Copyright (c) 2018-2023  Andrew C. Starritt
  *
  * The quaternion module is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * The quaternion module is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with the quaternion module.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Contact details:
